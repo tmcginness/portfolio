2e077566704b20e133aea6d720d2f366e2e05de8
Ted McGinness <ttmcginness22@gmail.com>
2022-01-27 15:25:04 -0600

    projects layout created, accordions added

---
+++ app.js
@@ -0,0 +1,19 @@
+$(() => {
+
+    const $acc = $(".accordion");
+
+
+    for (i = 0; i < $acc.length; i++) {
+        $acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            let panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+
+
+});
