b11910200eaf7354abfca410117b88d0093600f8
Ted McGinness <ttmcginness22@gmail.com>
2022-01-31 13:12:39 -0600

    completed media queries. site now fully responsive

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
 $(() => {
 
 
-    const $acc = $(".accordion");
+    const $acc1 = $("#project1");
+    const $acc2 = $("#project2");
+    const $acc3 = $("#project3");
+    const $nav = $(".topnav");
 
 
-    for (i = 0; i < $acc.length; i++) {
-        $acc[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            let panel = this.nextElementSibling;
-            if (panel.style.display === "block") {
-                panel.style.display = "none";
-            } else {
-                panel.style.display = "block";
-            }
-        });
-    }
+    $acc1.on("click", function () {
+
+        $acc1.css('display', 'block')
+        const $panel = $acc1.next();
+        $("#project1").toggleClass("active");
+        if ($panel.is(':visible')) {
+            $panel.css('display', 'none')
+        } else {
+            $panel.css('display', 'block')
+        }
+
+    });
+
+    $acc2.on("click", function () {
+
+        $acc2.css('display', 'block')
+        const $panel = $acc2.next();
+        $("#project2").toggleClass("active");
+        if ($panel.is(':visible')) {
+            $panel.css('display', 'none')
+        } else {
+            $panel.css('display', 'block')
+        }
+
+    });
+    $acc3.on("click", function () {
+
+        $acc3.css('display', 'block')
+        const $panel = $acc3.next();
+        $("#project3").toggleClass("active");
+        if ($panel.is(':visible')) {
+            $panel.css('display', 'none')
+        } else {
+            $panel.css('display', 'block')
+        }
+
+    });
+
+
+
+    $("#mobileMenu").on("click", function () {
+
+        $nav.toggleClass("responsive");
+    });
 
 
 });
